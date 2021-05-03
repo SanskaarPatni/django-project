9b9e072b8a620b92dea70f3805d967a05bc5bffb
Sanskaar Patni <patnisanskaar@gmail.com>
2021-05-03 23:45:39 +0530

    added a simple form

--- twt/urls.py
+++ twt/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('<int:id>', views.index1, name='index'),
+    path("home/", views.home, name="home"),
+    path("create/", views.create, name="create"),
     path('', views.home, name='home'),
 ]
--- twt/views.py
+++ twt/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .models import ToDoList
+from .forms import CreateNewList
 
 
 def index1(response, id):
@@ -9,3 +10,16 @@ def index1(response, id):
 
 def home(response):
     return render(response, 'twt/home.html', {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "twt/create.html", {"form": form})
