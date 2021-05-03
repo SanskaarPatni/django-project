5c221f65ca7fed97e8304e0c563ae8b4859a0bf6
Sanskaar Patni <patnisanskaar@gmail.com>
2021-05-03 23:19:59 +0530

    added custom html templates

--- twt/urls.py
+++ twt/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('<int:id>', views.index1, name='index'),
+    path('', views.home, name='home'),
 ]
--- twt/views.py
+++ twt/views.py
@@ -1,9 +1,11 @@
-# views.py file
-from django.http import HttpResponse
-from .models import ToDoList, Item
+from django.shortcuts import render
+from .models import ToDoList
 
 
-def index1(request, id):
+def index1(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br><p>%s</p>" % (ls.name, item.text))
+    return render(response, 'twt/list.html', {"ls": ls})
+
+
+def home(response):
+    return render(response, 'twt/home.html', {})
